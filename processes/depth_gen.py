9a16fcb41000740cd8b55da41c8f69e709cb3bc8
Mechi <158356934+MechkarovTUES@users.noreply.github.com>
2025-01-27 14:46:26 +0200

    depth generation update

--- processes/depth_gen.py
+++ processes/depth_gen.py
@@ -1,5 +1,5 @@
-from .config import BASELINE, FOCAL_PX
-from .utils import geo_coordinates_map
+from .config import BASELINE, FOCAL_PX, SCALE
+from .utils import geo_coordinates_map, geo_coordinates_map_2
 import numpy as np
 import cv2
 
@@ -10,12 +10,14 @@ def get_depth_map(disparity_map):
     for row in range(disparity_map.shape[0]):
         for col in range(disparity_map.shape[1]):
             disparity_Px = disparity_map[row, col]
-            # Calculate world coordinates using pre-derived formulas (similar triangles)
+            # Calculate world coordinates using pre-derived formulas (similar triangles). Z axis is inverted.
             if disparity_Px > 0:
-                Z = ((BASELINE * FOCAL_PX) / (disparity_Px)) * -1
+                # Z = ((BASELINE * FOCAL_PX) / (disparity_Px)) * -1
+                Z = disparity_Px
             else:
                 Z = 0
-            Y, X = geo_coordinates_map(cam = "right", x = col, y = row)
+                #@FIXME: Fix left/right swap.
+            Y, X = geo_coordinates_map(x = col, y = row)
             # Y, X = row, col
             depth[row, col] = [X, Y, Z]
         progress = int((row / disparity_map.shape[0]) * 100)
